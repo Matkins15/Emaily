a8d79f79c582c5a35ced117aadcf6d96ba09cf19
Matthew Atkins <matkinsdesigns@gmail.com>
2018-12-13 00:23:14 -0500

    google oauth20

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-// '/' === route route
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback"
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["profile", "email"]
+	})
+);
 
 // dynamic port: environment variable used by heroku to indentify a port || locally on port 5000
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
