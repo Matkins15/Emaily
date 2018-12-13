b3eee87949fc70bd99d761ffcf48d0eb72ae58ff
Matthew Atkins <matkinsdesigns@gmail.com>
2018-12-12 23:04:49 -0500

    typo in index.js file

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 // dynamic port: environment variable used by heroku to indentify a port || locally on port 5000
-const PORT = process.eventNames.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
